9b8cac15413e43300ddea00cd8fbf3aa45367477
nezu <29180158+dumbasPL@users.noreply.github.com>
2023-06-03 21:00:36 +0200

    add debug hook

---
+++ hook/RTTI.cpp
@@ -0,0 +1,132 @@
+/*
+MIT License
+
+Copyright (c) 2021 Sam
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#include "RTTI.h"
+
+static void* FindData(void* pBeginAddress, void* pEndAddress, void* pData, size_t unDataSize) {
+	unsigned char* pBegin = reinterpret_cast<unsigned char*>(pBeginAddress);
+	const unsigned char* pEnd = reinterpret_cast<unsigned char*>(pEndAddress);
+	const uintptr_t unEnd = reinterpret_cast<uintptr_t>(pEnd) - unDataSize;
+	for (uintptr_t i = 0; (i < reinterpret_cast<uintptr_t>(pEnd)) && (pBegin <= reinterpret_cast<void*>(unEnd)); ++i, ++pBegin) {
+		uintptr_t unNextStart = 0;
+		uintptr_t unResult = 0;
+		bool bSuccess = true;
+		for (size_t j = 0; j < unDataSize; ++j) {
+			if (reinterpret_cast<unsigned char*>(pData)[j] == 0x2A) {
+				continue;
+			}
+			const unsigned char unSymbol = pBegin[j];
+			if (unSymbol == reinterpret_cast<unsigned char*>(pData)[0]) {
+				unNextStart = j;
+			}
+			if (unSymbol != reinterpret_cast<unsigned char*>(pData)[j]) {
+				unResult = unNextStart;
+				bSuccess = false;
+				break;
+			}
+		}
+		if (bSuccess) {
+			return reinterpret_cast<void*>(pBegin);
+		} else {
+			i += unResult;
+		}
+	}
+	return nullptr;
+}
+
+void* FindRTTI(void* pBegin, void* pEnd, const char* szName) {
+	if (!pBegin || !pEnd || !szName) {
+		return nullptr;
+	}
+	void* pType = FindData(pBegin, pEnd, const_cast<char*>(szName), strnlen_s(szName, 0x7FF));
+	if (!pType) {
+		return nullptr;
+	}
+	void* pLastReference = pBegin;
+	while (pLastReference < pEnd) {
+#ifdef _WIN64
+		uintptr_t unTypeOffsetTemp = reinterpret_cast<uintptr_t>(reinterpret_cast<char*>(pType) - sizeof(void*) * 2 - reinterpret_cast<uintptr_t>(pBegin));
+		uint32_t unTypeOffset = (*(reinterpret_cast<unsigned int*>(&unTypeOffsetTemp)));
+		char* pReference = reinterpret_cast<char*>(FindData(pLastReference, pEnd, &unTypeOffset, sizeof(unTypeOffset)));
+#elif _WIN32
+		char* pTypeDescriptor = reinterpret_cast<char*>(pType) - sizeof(void*) * 2;
+		char* pReference = reinterpret_cast<char*>(FindData(pLastReference, pEnd, &pTypeDescriptor, sizeof(pTypeDescriptor)));
+#endif
+		if (!pReference) {
+			break;
+		}
+#ifdef _WIN64
+		if (!(((*(reinterpret_cast<uint32_t*>(pReference))) != 0) && ((*(reinterpret_cast<uint32_t*>(pReference + sizeof(uint32_t))) != 0)))) {
+			pLastReference = pReference + sizeof(void*);
+			continue;
+		}
+#elif _WIN32
+		if (!(((*(reinterpret_cast<uint32_t*>(pReference))) >= reinterpret_cast<uint32_t>(pBegin)) && ((*(reinterpret_cast<uint32_t*>(pReference + sizeof(uint32_t))) >= reinterpret_cast<uint32_t>(pBegin))))) {
+			pLastReference = pReference + sizeof(void*);
+			continue;
+		}
+#endif
+		char* pLocation = pReference - sizeof(uint32_t) * 3;
+		char* pMeta = reinterpret_cast<char*>(FindData(pBegin, pEnd, &pLocation, sizeof(pLocation)));
+		if (!pMeta) {
+			pLastReference = pReference + sizeof(void*);
+			continue;
+		}
+		return reinterpret_cast<void*>(pMeta + sizeof(void*));
+	}
+	return nullptr;
+}
+
+void* FindRTTI(HMODULE hModule, const char* szName) {
+	if (!hModule || !szName) {
+		return nullptr;
+	}
+	MODULEINFO modinf;
+	if (!GetModuleInformation(reinterpret_cast<HANDLE>(-1), hModule, &modinf, sizeof(MODULEINFO))) {
+		return nullptr;
+	}
+	char* pBegin = reinterpret_cast<char*>(modinf.lpBaseOfDll);
+	return FindRTTI(pBegin, pBegin + modinf.SizeOfImage, szName);
+}
+
+void* FindRTTI(const char* szModule, const char* szName) {
+	if (!szModule || !szName) {
+		return nullptr;
+	}
+	HMODULE hModule = GetModuleHandleA(szModule);
+	if (!hModule) {
+		return nullptr;
+	}
+	return FindRTTI(hModule, szName);
+}
+
+void* FindRTTI(const wchar_t* szModule, const char* szName) {
+	if (!szModule || !szName) {
+		return nullptr;
+	}
+	HMODULE hModule = GetModuleHandleW(szModule);
+	if (!hModule) {
+		return nullptr;
+	}
+	return FindRTTI(hModule, szName);
+}
---
+++ hook/hook.cpp
@@ -0,0 +1,166 @@
+#include <Windows.h>
+#include <shlwapi.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include "RTTI.h"
+#include "minhook/MinHook.h"
+
+#ifdef _WIN32
+#define TC_ECX void* edx,
+#define STEAMCLIENT_DLL L"steamclient.dll"
+#else
+#define TC_ECX
+#define STEAMCLIENT_DLL L"steamclient64.dll"
+#endif
+
+template <class T>
+class utl_memory {
+public:
+    T* memory;
+    int allocation_count;
+    int grow_size;
+};
+
+template <class T>
+class utl_buffer {
+public:
+    void* unk;
+    utl_memory<T> memory;
+    int m_Get;
+    int m_Put;
+};
+
+std::string hexStr(uint8_t* data, int len) {
+  std::stringstream ss;
+  ss << std::hex;
+  for (int i = 0; i < len; ++i)
+    ss << std::setw(2) << std::setfill('0') << (int)data[i];
+  return ss.str();
+}
+
+typedef void*(__thiscall* ReadPipe_t)(void* ecx, utl_buffer<uint8_t>* buffer);
+ReadPipe_t ReadPipe_o;
+bool __fastcall ReadPipe(void* ecx, TC_ECX utl_buffer<uint8_t>* buffer) {
+  bool ret = ReadPipe_o(ecx, buffer);
+  std::cout << "[HOOK] Read:  " << hexStr(buffer->memory.memory, buffer->m_Put) << std::endl;
+  return ret;
+}
+
+typedef bool(__thiscall* PeakPipe_t)(void* ecx, uint8_t* command);
+PeakPipe_t PeakPipe_o;
+bool __fastcall PeakPipe(void* ecx, TC_ECX uint8_t* command) {
+  bool ret = PeakPipe_o(ecx, command);
+  std::cout << "[HOOK] Peak:  " << ret << " " << (command ? (int)*command : -1) << std::endl;
+  return ret;
+}
+
+typedef void*(__thiscall* WritePipe_t)(void* ecx, utl_buffer<uint8_t>* buffer, bool notify);
+WritePipe_t WritePipe_o;
+bool __fastcall WritePipe(void* ecx, TC_ECX utl_buffer<uint8_t>* buffer, bool notify) {
+  std::cout << "[HOOK] Write: " << hexStr(buffer->memory.memory, buffer->m_Put) << " " << notify << std::endl;
+  return WritePipe_o(ecx, buffer, notify);
+}
+
+typedef bool(__thiscall* WaitMsg_t)(void* ecx, DWORD timeout);
+WaitMsg_t WaitMsg_o;
+bool __fastcall WaitMsg(void* ecx, TC_ECX DWORD timeout) {
+  bool ret = WaitMsg_o(ecx, timeout);
+  std::cout << "[HOOK] WaitMsg: " << timeout << " " << ret << std::endl;
+  return ret;
+}
+
+typedef bool(__thiscall* ResetEventS_t)(void* ecx);
+ResetEventS_t ResetEventS_o;
+bool __fastcall ResetEventS(void* ecx) {
+  bool ret = ResetEventS_o(ecx);
+  std::cout << "[HOOK] ResetEvent: " << ret << std::endl;
+  return ret;
+}
+
+typedef void(__thiscall* SetEventS_t)(void* ecx, bool set);
+SetEventS_t SetEventS_o;
+void __fastcall SetEventS(void* ecx, TC_ECX bool set) {
+  std::cout << "[HOOK] SetEvent: " << set << std::endl;
+  SetEventS_o(ecx, set);
+}
+
+bool init(HMODULE hSteamClient) {
+  void** table = (void**)FindRTTI(hSteamClient, ".?AVCCrossProcessPipe@@");
+  if (!table) {
+    std::cout << "Failed to find CCrossProcessPipe VTable" << std::endl;
+    return false;
+  }
+  std::cout << "CCrossProcessPipe VTable: " << table << std::endl;
+
+  if (MH_CreateHook(table[5], &WritePipe, (void**)&WritePipe_o) != MH_OK) {
+    std::cout << "Failed to hook CCrossProcessPipe::WritePipe" << std::endl;
+    return false;
+  }
+
+  if (MH_CreateHook(table[6], &PeakPipe, (void**)&PeakPipe_o) != MH_OK) {
+    std::cout << "Failed to hook CCrossProcessPipe::PeakPipe" << std::endl;
+    return false;
+  }
+
+  if (MH_CreateHook(table[7], &ReadPipe, (void**)&ReadPipe_o) != MH_OK) {
+    std::cout << "Failed to hook CCrossProcessPipe::ReadPipe" << std::endl;
+    return false;
+  }
+
+  if (MH_CreateHook(table[8], &WaitMsg, (void**)&WaitMsg_o) != MH_OK) {
+    std::cout << "Failed to hook CCrossProcessPipe::WaitMsg" << std::endl;
+    return false;
+  }
+
+  if (MH_CreateHook(table[9], &ResetEventS, (void**)&ResetEventS_o) != MH_OK) {
+    std::cout << "Failed to hook CCrossProcessPipe::ResetEvent" << std::endl;
+    return false;
+  }
+
+  if (MH_CreateHook(table[10], &SetEventS, (void**)&SetEventS_o) != MH_OK) {
+    std::cout << "Failed to hook CCrossProcessPipe::SetEvent" << std::endl;
+    return false;
+  }
+
+  if (MH_EnableHook(MH_ALL_HOOKS) != MH_OK) {
+    std::cout << "Failed to enable hooks" << std::endl;
+    return false;
+  }
+
+  std::cout << "Hooks enabled" << std::endl;
+  return true;
+}
+
+typedef HMODULE(WINAPI* LoadLibraryExW_t)(LPCWSTR lpLibFileName, HANDLE hFile, DWORD dwFlags);
+LoadLibraryExW_t LoadLibraryExW_o;
+HMODULE WINAPI LoadLibraryExW_h(LPCWSTR lpLibFileName, HANDLE hFile, DWORD dwFlags) {
+  HMODULE ret = LoadLibraryExW_o(lpLibFileName, hFile, dwFlags);
+  if (wcscmp(PathFindFileNameW(lpLibFileName), STEAMCLIENT_DLL) == 0) {
+    init(ret);
+  }
+  return ret;
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+  switch (fdwReason) {
+  case DLL_PROCESS_ATTACH:
+    DisableThreadLibraryCalls(hinstDLL);
+    if (MH_Initialize() != MH_OK)
+      return FALSE;
+    if (MH_CreateHookApi(L"kernel32.dll", "LoadLibraryExW", LoadLibraryExW_h, (void**)&LoadLibraryExW_o) != MH_OK)
+      return FALSE;
+    if (MH_EnableHook(MH_ALL_HOOKS) != MH_OK)
+      return FALSE;
+    break;
+  case DLL_PROCESS_DETACH:
+    if (MH_DisableHook(MH_ALL_HOOKS) != MH_OK)
+      return FALSE;
+    if (MH_Uninitialize() != MH_OK)
+      return FALSE;
+    break;
+  }
+  return TRUE;
+}
+ 
